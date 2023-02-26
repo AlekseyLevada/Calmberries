36ca31598162a744c921db988e00caaa1202a81b
Aleksey <aleksey.levada@gmail.com>
2023-02-26 13:25:56 +0300

    'update-publicpath'

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  publicPath:'/Calmberries/',
+  publicPath:'/Calmberries',
   //publicDir:'/Calmberries/',
   //base:'/Calmberries/',
   plugins: [vue()]
