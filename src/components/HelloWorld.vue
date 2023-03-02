c235ec92db89cd25dcf289c6b28fd22473e41895
Aleksey <aleksey.levada@gmail.com>
2023-03-02 12:13:05 +0300

    update-styles-main-window

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -5,6 +5,9 @@
 <template>
   <div class="switch">
     <h1>Сalmberries</h1>
+    <h3>
+      Доступные цены
+    </h3>
     <div class="link__container">
       <router-link to="/news">
         <div class="detailBtn">
@@ -16,28 +19,28 @@
 </template>
 
 <style scoped>
-h1{
+h1 {
   font-family: 'Pacifico', cursive;
   font-size: 90px;
-  line-height: 240px;
-  margin-bottom: 120px;
 }
-h1::before {
-  content: 'Свободные цены';
-  position: absolute;
-  top: 35%;
-  left: 45%;
+
+h3 {
   font-size: 24px;
+  font-family: 'Pacifico', cursive;
+  margin-bottom: 120px;
+  text-align: center;
+  color: beige;
 }
+
 .link__container {
   display: flex;
   justify-content: center;
   align-items: center;
 }
-a div.detailBtn{
+
+a div.detailBtn {
   width: 200px;
   padding: 5px;
   font-family: 'Pacifico', cursive;
   font-size: 18px;
-}
-</style>
+}</style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,4 @@
 import { createApp } from 'vue'
-import './style.css'
 import './scss/my_style.css'
 import App from './App.vue'
 import router from './utils/router'
